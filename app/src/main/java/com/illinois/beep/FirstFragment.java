12d7f024aca1b7124308f2212e709ce489fb110e
Harsh Deep <hrshdeep51@gmail.com>
2021-11-15 16:24:07 -0600

    Add portrait barcode scanning

--- app/src/main/java/com/illinois/beep/FirstFragment.java
+++ app/src/main/java/com/illinois/beep/FirstFragment.java
@@ -6,12 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.zxing.integration.android.IntentIntegrator;
 import com.illinois.beep.databinding.FragmentFirstBinding;
+
+import com.journeyapps.*;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class FirstFragment extends Fragment {
 
@@ -44,11 +50,25 @@ public class FirstFragment extends Fragment {
         ImageButton cameraBtn = binding.cameraButton;
         ImageButton editBtn = binding.cameraButton;
 
+        IntentIntegrator integrator = new IntentIntegrator(getActivity());
+        integrator.setOrientationLocked(false); // don't force landscape
+
+
+//        final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+//                result -> {
+//                    if(result.getContents() == null) {
+//                        Toast.makeText(MyActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
+//                    } else {
+//                        Toast.makeText(MyActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+//                    }
+//                });
+
         // we can add more listeners if needed
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(LOG_TAG, "Camera button clicked");
+                integrator.initiateScan();
             }
         });
     }
