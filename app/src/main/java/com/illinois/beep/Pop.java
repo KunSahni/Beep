69f7ae0ed22c9f1b46a1f4c5fb4cfef84cc5f080
emmayu <emmayu2@illinois.edu>
2021-11-18 00:16:23 -0600

    Added popup

---
+++ app/src/main/java/com/illinois/beep/Pop.java
@@ -0,0 +1,22 @@
+package com.illinois.beep;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+
+public class Pop extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popup_window);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width * 0.8), (int)(height*0.6));
+    }
+}
