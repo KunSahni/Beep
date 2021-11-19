33f31725cc4c0f56f594aa73202ecfed8d64d0c4
emmayu <emmayu2@illinois.edu>
2021-11-18 23:09:44 -0600

    fixed binding of profile

--- app/src/main/java/com/illinois/beep/FirstFragment.java
+++ app/src/main/java/com/illinois/beep/FirstFragment.java
@@ -68,7 +68,7 @@ public class FirstFragment extends Fragment {
                     .navigate(R.id.action_FirstFragment_to_TestFragment);
         });
 
-        binding.textviewFirst.setOnClickListener(new View.OnClickListener() {
+        binding.peopleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
