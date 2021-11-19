e786982fe9f42ec0255e22ed5f2e50cfbe2e517d
Harsh Deep <hrshdeep51@gmail.com>
2021-11-18 22:26:02 -0600

    Add basic profile data

---
+++ app/src/main/java/com/illinois/beep/database/ProfileInfo.java
@@ -0,0 +1,35 @@
+package com.illinois.beep.database;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProfileInfo {
+  static Map<String, List<Preferance>> prefMap;
+
+  public static void setupSampleInfo() {
+    prefMap = new HashMap<>();
+    addPerson("Myself");
+    addPerson("Allie");
+    addPerson("Jim");
+    addPerson("Alex");
+    addPerson("Sarah");
+
+    // make myself lactose intolerant
+    addRestriction("Myself", "lactose", true);
+
+    // make allie vegan
+    addRestriction("Allie", "vegan", false);
+  }
+
+  public static void addRestriction(String personName, String whichRestriction, boolean important) {
+    assert prefMap != null;
+
+    prefMap.get(personName).add(new Preferance(whichRestriction, important));
+  }
+
+  public static void addPerson(String personName) {
+    prefMap.put(personName, new ArrayList<>());
+  }
+}
