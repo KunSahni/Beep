36d749d314b2d730ec4475e4663fcab6c91bca22
Hsin-Yu Huang <brayce1996@gmail.com>
2021-12-02 17:27:52 -0600

    Setup swipe delete animation

--- app/src/main/java/com/illinois/beep/MyListAdapter.java
+++ app/src/main/java/com/illinois/beep/MyListAdapter.java
@@ -1,6 +1,13 @@
 package com.illinois.beep;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,6 +25,7 @@ import java.util.List;
 import java.util.Set;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
@@ -44,9 +52,23 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
 
     public static class SwipeToDeleteCallback extends ItemTouchHelper.SimpleCallback {
         private final MyListAdapter mAdapter;
+        private final Paint mClearPaint;
+        private final ColorDrawable mBackground;
+        private final int backgroundColor;
+        private final Drawable deleteDrawable;
+        private final int intrinsicWidth;
+        private final int intrinsicHeight;
+
         public SwipeToDeleteCallback(MyListAdapter adapter) {
             super(0,ItemTouchHelper.LEFT);
             mAdapter = adapter;
+            mBackground = new ColorDrawable();
+            backgroundColor = Color.parseColor("#b80f0a");
+            mClearPaint = new Paint();
+            mClearPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+            deleteDrawable = ContextCompat.getDrawable(adapter.fragment.getContext(), R.drawable.trash_alt_solid);
+            intrinsicWidth = deleteDrawable.getIntrinsicWidth();
+            intrinsicHeight = deleteDrawable.getIntrinsicHeight();
         }
 
         @Override
@@ -58,6 +80,45 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
             int position = viewHolder.getAdapterPosition();
             mAdapter.deleteItem(position);
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+            View itemView = viewHolder.itemView;
+            int itemHeight = itemView.getHeight();
+
+            boolean isCancelled = dX == 0 && !isCurrentlyActive;
+
+            if (isCancelled) {
+                clearCanvas(c, itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                return;
+            }
+
+            mBackground.setColor(backgroundColor);
+            mBackground.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+            mBackground.draw(c);
+
+            int deleteIconTop = itemView.getTop() + (itemHeight - intrinsicHeight) / 2;
+            int deleteIconMargin = (itemHeight - intrinsicHeight) / 2;
+            int deleteIconLeft = itemView.getRight() - deleteIconMargin - intrinsicWidth;
+            int deleteIconRight = itemView.getRight() - deleteIconMargin;
+            int deleteIconBottom = deleteIconTop + intrinsicHeight;
+
+
+            deleteDrawable.setBounds(deleteIconLeft, deleteIconTop, deleteIconRight, deleteIconBottom);
+            deleteDrawable.draw(c);
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+
+        }
+
+        private void clearCanvas(Canvas c, Float left, Float top, Float right, Float bottom) {
+            c.drawRect(left, top, right, bottom, mClearPaint);
+
         }
     };
 
