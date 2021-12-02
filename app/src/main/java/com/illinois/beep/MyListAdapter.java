47e6e2af64140043de3e9f7f28a59a6ae9e924e5
Hsin-Yu Huang <brayce1996@gmail.com>
2021-12-02 12:31:18 -0600

    Adjust MyList UI

--- app/src/main/java/com/illinois/beep/MyListAdapter.java
+++ app/src/main/java/com/illinois/beep/MyListAdapter.java
@@ -6,8 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.illinois.beep.database.Product;
@@ -17,12 +17,29 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import androidx.annotation.RequiresApi;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MyListAdapter extends BaseAdapter {
+public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder> {
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout myListItem;
+        ImageView productImage;
+        TextView productName;
+        TextView productQuantity;
+        ImageView restrictionIcon;
+
+        public ViewHolder(View view) {
+            super(view);
+            myListItem = view.findViewById(R.id.my_list_item);
+            productImage = view.findViewById(R.id.product_image);
+            productName = view.findViewById(R.id.product_name);
+            productQuantity = view.findViewById(R.id.product_quantity_num);
+            restrictionIcon = view.findViewById(R.id.restriction_icon);
+        }
+    }
 
     enum RestrictionLevel {
         GOOD,
@@ -30,73 +47,51 @@ public class MyListAdapter extends BaseAdapter {
         DANGER,
     };
 
-    Context context;
-    FragmentActivity activity;
     Fragment fragment;
     List<MyListItem> myList;
     Set<String> dangerRestrictions = new HashSet<>();
     Set<String> warningRestrictions = new HashSet<>();
 
-    public MyListAdapter(Context context, FragmentActivity activity, Fragment fragment, List<MyListItem> myList) {
-        this.context = context;
-        this.activity = activity;
+    public MyListAdapter(Fragment fragment, List<MyListItem> myList) {
         this.fragment = fragment;
         this.myList = myList;
     }
 
+    @NonNull
     @Override
-    public int getCount() {
-        return myList.size();
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_list_item, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
-    public Object getItem(int position) {
-        return myList.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder;
-
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
         MyListItem item = myList.get(position);
         Product product = item.getProduct();
 
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.my_list_item, parent, false);
-            viewHolder = new ViewHolder(convertView);
-            convertView.setTag(viewHolder);
-            convertView.setOnClickListener(v -> {
-                System.out.println(position + "th item clicked");
-                Bundle bundle = new Bundle();
-                bundle.putString("productId", product.getId());
-                bundle.putInt("position", position);
-                bundle.putInt("quantity", item.quantity);
-                NavHostFragment.findNavController(fragment)
-                        .navigate(R.id.action_FirstFragment_to_productReviewFragment, bundle);
-            });
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
+        viewHolder.myListItem.setOnClickListener(v -> {
+            Bundle bundle = new Bundle();
+            bundle.putString("productId", product.getId());
+            bundle.putInt("position", position);
+            bundle.putInt("quantity", item.quantity);
+            NavHostFragment.findNavController(fragment)
+                    .navigate(R.id.action_FirstFragment_to_productReviewFragment, bundle);
+        });
 
 
         Picasso.get().load(product.getImage_url()).into(viewHolder.productImage);
         RestrictionLevel restrictionLevel = RestrictionLevel.GOOD;
 
         for (String restriction: warningRestrictions) {
-            if (Boolean.TRUE.equals(product.getIndications().getOrDefault(restriction, false))) {
+            if (Boolean.TRUE.equals(product.getIndications().get(restriction))) {
                 restrictionLevel = RestrictionLevel.WARN;
                 break;
             }
         }
 
         for (String restriction: dangerRestrictions) {
-            if (Boolean.TRUE.equals(product.getIndications().getOrDefault(restriction, false))) {
+            if (Boolean.TRUE.equals(product.getIndications().get(restriction))) {
                 restrictionLevel = RestrictionLevel.DANGER;
                 break;
             }
@@ -118,7 +113,16 @@ public class MyListAdapter extends BaseAdapter {
         viewHolder.productName.setText(product.getName());
         viewHolder.productQuantity.setText(item.getQuantity().toString());
 
-        return convertView;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return myList.size();
     }
 
     public void updateMyList(List<MyListItem> myList) {
@@ -130,22 +134,5 @@ public class MyListAdapter extends BaseAdapter {
         this.warningRestrictions = warningRestrictions;
         this.dangerRestrictions = dangerRestrictions;
         notifyDataSetChanged();
-    }
-
-    /**
-     * design pattern reference: https://guides.codepath.com/android/Using-a-BaseAdapter-with-ListView#create-the-custom-baseadapter-implementation
-     */
-    private class ViewHolder {
-        ImageView productImage;
-        TextView productName;
-        TextView productQuantity;
-        ImageView restrictionIcon;
-
-        public ViewHolder(View view) {
-            productImage = view.findViewById(R.id.product_image);
-            productName = view.findViewById(R.id.product_name);
-            productQuantity = view.findViewById(R.id.product_quantity_num);
-            restrictionIcon = view.findViewById(R.id.restriction_icon);
-        }
     }
 }
